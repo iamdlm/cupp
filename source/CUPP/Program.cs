e620140db5ddd3a5e5017724fbb93dd615cdd522
iamdlm <diogoluismesquita@outlook.com>
2020-07-26 23:14:48 +0100

    Add arguments

--- source/CUPP/Program.cs
+++ source/CUPP/Program.cs
@@ -33,23 +33,36 @@ namespace CUPP
                             string combo1 = $"{fstWord}{sndWord}";
                             if (ValidateRequirements(combo1)) { passwords.Add(combo1); }
 
-                            // first word + special char + second word
-                            string combo2 = $"{fstWord}{specialChar}{sndWord}";
-                            if (ValidateRequirements(combo2)) { passwords.Add(combo2); }
-
-                            foreach (string number in numbers)
+                            // s - special chars
+                            if (args.Contains("s"))
                             {
-                                // first word + second word + number
-                                string combo3 = $"{fstWord}{sndWord}{number}";
-                                if (ValidateRequirements(combo3)) { passwords.Add(combo3); }
+                                // first word + special char + second word
+                                string combo2 = $"{fstWord}{specialChar}{sndWord}";
+                                if (ValidateRequirements(combo2)) { passwords.Add(combo2); }
 
-                                // first word + special char + second word + number
-                                string combo4 = $"{fstWord}{specialChar}{sndWord}{number}";
-                                if (ValidateRequirements(combo4)) { passwords.Add(combo4); }
+                            }
 
-                                // first word + second word + special char + number
-                                string combo5 = $"{fstWord}{sndWord}{specialChar}{number}";
-                                if (ValidateRequirements(combo5)) { passwords.Add(combo5); }
+                            // n - numbers
+                            if (args.Contains("n"))
+                            {
+                                foreach (string number in numbers)
+                                {
+                                    // first word + second word + number
+                                    string combo3 = $"{fstWord}{sndWord}{number}";
+                                    if (ValidateRequirements(combo3)) { passwords.Add(combo3); }
+
+                                    // s - special chars
+                                    if (args.Contains("s"))
+                                    {
+                                        // first word + special char + second word + number
+                                        string combo4 = $"{fstWord}{specialChar}{sndWord}{number}";
+                                        if (ValidateRequirements(combo4)) { passwords.Add(combo4); }
+
+                                        // first word + second word + special char + number
+                                        string combo5 = $"{fstWord}{sndWord}{specialChar}{number}";
+                                        if (ValidateRequirements(combo5)) { passwords.Add(combo5); }
+                                    }
+                                }
                             }
                         }
                     }
@@ -57,52 +70,60 @@ namespace CUPP
             }
 
             List<string> passwordsInverted = new List<string>();
+            List<string> passwordsCapital = new List<string>();
 
             foreach (string password in passwords)
             {
-                foreach (string specialChar in chars)
+                // c - capitalization
+                if (args.Contains("c") && char.IsLetter(password[0]))
                 {
-                    if (password.Contains(specialChar))
+                    passwordsCapital.Add(FirstCharToUpper(password));
+                }
+
+                // s - special chars
+                if (args.Contains("i") && args.Contains("s"))
+                {
+                    foreach (string specialChar in chars)
                     {
-                        string[] segments = password.Split(specialChar.ToCharArray()[0]);
-                        passwordsInverted.Add($"{segments[1]}{specialChar}{segments[0]}");
+                        if (password.Contains(specialChar))
+                        {
+                            string[] segments = password.Split(specialChar.ToCharArray()[0]);
+                            passwordsInverted.Add($"{segments[1]}{specialChar}{segments[0]}");
+                        }
                     }
                 }
             }
 
-            passwords.AddRange(passwordsInverted);
+            if (args.Contains("i")) { passwords.AddRange(passwordsInverted); }
+            if (args.Contains("c")) { passwords.AddRange(passwordsCapital); }
 
             File.WriteAllLines("passwords.txt", passwords);
         }
 
-        private static bool ValidateRequirements(string password, int minChecks = 2)
+        private static bool ValidateRequirements(string password)
         {
-            int checkCounts = 0;
+            bool result = true;
 
-            // validate min length
-            if (password.Length >= minLength)
-                checkCounts++;
+            // validate length
+            if (password.Length < minLength)
+                result = false;
 
-            // validate max length
-            if (maxLength == 0)
-                checkCounts++;
-            else if (password.Length <= maxLength)
-                checkCounts++;
-
+            if (maxLength != 0 && password.Length > maxLength)
+                result = false;
 
             // validate only numbers
-            if (!IsDigitsOnly(password))
-                checkCounts++;
+            if (IsDigitsOnly(password))
+                result = false;
 
             // validate only letters
-            if (!IsLettersOnly(password))
-                checkCounts++;
+            if (IsLettersOnly(password))
+                result = false;
 
-            // validate +1 upper case letter(s)
-            if (!password.Equals(password.ToLower()))
-                checkCounts++;
+            //// validate upper case letter(s)
+            //if (password.Equals(password.ToLower()))
+            //    result = false;
 
-            return checkCounts >= minChecks ? true : false;
+            return result;
         }
 
         private static bool IsDigitsOnly(string str)
@@ -151,6 +172,11 @@ namespace CUPP
             }
 
             return true;
+        }
+
+        private static string FirstCharToUpper(string input)
+        {
+            return input.First().ToString().ToUpper() + input.Substring(1);
         }
     }
 }
